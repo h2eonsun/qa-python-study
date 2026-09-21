4eb8e1553b9c612c7bc3f25721cc41d394974ec0
unknown <h2eonsun@gmail.com>
2026-09-21 23:44:26 +0900

    docs : add development environment setup

---
+++ environment.py
@@ -0,0 +1,4 @@
+print("Python : Ready")
+print("Git : Ready")
+print("GitHub : Ready")
+print("VS Code : Ready")
--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, QA Python!")
+name = "박현선"
+job = "QA"
+tool = "Python"
+print("이름 : ", name)
+print("목표 직무 : ", job)
+print("학습 기술 : ", tool)
